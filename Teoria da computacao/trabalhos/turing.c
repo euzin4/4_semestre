caedbd9b13619a5178043a07976c502278325540
kevin willian <tste@outlook.com.br>
2018-09-05 17:25:06 -0300

    terminar

--- Teoria da computacao/trabalhos/turing.c
+++ Teoria da computacao/trabalhos/turing.c
@@ -1,24 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct _celula{
+    char novoc;
+    int estado;
+    int posicao;
+}celula;
+
 int main()
 {
-    char val[30];
-    int car;
+    celula matriz[8][4];
+    char val[50];
+    int car, cont, l, c;
+
+    //inicia matriz
+    for(l=0; l<8; l++){
+        for(c=0; c<4; c++){
+            matriz[l][c].novoc = ''
+        }
+    }
+    matriz[0][0].novoc = '>'; matriz[0][0].estado = 0; matriz[0][0].posicao = 1;
+    matriz[0][1].novoc = '>'; matriz[0][1].estado = 2; matriz[0][1].posicao = 1;
+    matriz[0][2].novoc = '>'; matriz[0][2].estado = 1; matriz[0][2].posicao = 1;
+    matriz[0][3].novoc = 'U'; matriz[0][3].estado = 6; matriz[0][3].posicao = 0;
+    matriz[1][1].novoc = '0'; matriz[1][1].estado = 1; matriz[1][1].posicao = 1;
+    matriz[1][2].novoc = '1'; matriz[1][2].estado = 1; matriz[1][2].posicao = 1;
+    matriz[1][3].novoc = 'U'; matriz[1][3].estado = 3; matriz[1][3].posicao = -1;
+    matriz[2][1].novoc = '0'; matriz[2][1].estado = 2; matriz[2][1].posicao = 1;
+    matriz[2][2].novoc = '1'; matriz[2][2].estado = 2; matriz[2][2].posicao = 1;
+    matriz[2][3].novoc = 'U'; matriz[2][3].estado = 4; matriz[2][3].posicao = -1;
+    matriz[3][0].novoc = '>'; matriz[3][0].estado = 6; matriz[3][0].posicao = 0;
+    matriz[3][1].novoc = '0'; matriz[3][1].estado = 7; matriz[3][1].posicao = 0;
+    matriz[3][2].novoc = 'U'; matriz[3][2].estado = 5; matriz[3][2].posicao = -1;
+    matriz[4][0].novoc = '>'; matriz[4][0].estado = 6; matriz[4][0].posicao = 0;
+    matriz[4][1].novoc = 'U'; matriz[4][1].estado = 5; matriz[4][1].posicao = -1;
+    matriz[4][2].novoc = '1'; matriz[4][2].estado = 7; matriz[4][2].posicao = 0;
+    matriz[5][0].novoc = '>'; matriz[5][0].estado = 0; matriz[5][0].posicao = 1;
+    matriz[5][1].novoc = '0'; matriz[5][1].estado = 5; matriz[5][1].posicao = -1;
+    matriz[5][2].novoc = '1'; matriz[5][2].estado = 5; matriz[5][2].posicao = -1;
 
     printf("valor:");
-    fgets(val, 30, stdin);
-    car = strlen(val);
-
+    fgets(val, 49, stdin);
+    car = strlen(val);    //quantidade de caracteres
+    cont = car;
+    //adição do 'U'
+    val[cont+1] = val[cont-1];
+    val[cont-1] = 'U';
+    cont+1;
+    //adição do '>'
     while(1){
-        val[car] = val[car-1];
-        car--;
-        if(car == 0){
+        val[cont] = val[cont-1];
+        cont--;
+        if(cont == 0){
             val[0] = '>';
             break;
         }
     }
-
-    printf("%d\n",car);
-    printf("%c",val[1]);
+    //----
+    printf("%d\n",car-1);
+    printf("%s",val);
 }
