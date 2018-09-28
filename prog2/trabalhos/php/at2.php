adab7a5bb68f7da1a8b41508da2e14b98e7ff8a2
kevin willian <tste@outlook.com.br>
2018-09-27 22:00:23 -0300

    Update at2.php

--- prog2/trabalhos/php/at2.php
+++ prog2/trabalhos/php/at2.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Atividade PHP</title>
 	<style type="text/css">
 		table, tr, td, th{
 			border: 1px solid black;
@@ -16,6 +16,38 @@
 	</style>
 </head>
 <body>
+	<?php
+		function horario(){
+			$hora = date('G');
+			if($hora >= 0 && $hora < 7 ){
+			    echo "Boa Madrugada ";
+			}else if($hora >= 7 && $hora < 12){
+			    echo "Bom Dia ";
+			}else if($hora >= 12 && $hora < 18){
+			    echo "Boa Tarde ";
+			}else{
+			    echo "Boa noite ";
+			}
+		}
+		function diaSemana(){
+			$dia = date('D');
+			if($dia == 'sun'){
+			    echo "Domingo ";
+			}else if($dia == 'Mon'){
+			    echo "Segunda ";
+			}else if($dia == 'Tue'){
+			    echo "Terça ";
+			}else if($dia == 'Wed'){
+			    echo "Quarta ";
+			}else if($dia == 'Thu'){
+			    echo "Quinta ";
+			}else if($dia == 'Fri'){
+			    echo "Sexta ";
+			}else{
+			    echo "Sábado ";
+			}
+		}
+	?>
 	<?php
 		$alunos = array(
 			array("nome" => "joao", "np1" => 7,"np2" => 5),
